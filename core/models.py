5d129fa192722bf855717e8f35800d01f4f15392
Gathvik <gathvik.nk@gmail.com>
2024-09-03 23:31:32 +0530

    Add: Model for tasks

--- core/models.py
+++ core/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 from django.db.models import Model
+
+class Tasks(Model):
+    task = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'task_list'
